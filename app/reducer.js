d20d5b0fbeed55000699dbaf0e26a4df0161a5d3
Irene Robb <IreneRobb@Irenes-MacBook-Pro.local>
2016-02-09 15:17:58 +1300

    adding 1/2 of new function

--- app/reducer.js
+++ app/reducer.js
@@ -8,7 +8,8 @@ const INITIAL_STATE = fromJS({
     {id: 4, name:'car'},
     {id: 5, name:'falcon'}
   ],
-  cart: [1,4]
+  cart: [1,4],
+  amountOfProducts: 0
 })
 
 export default (state = INITIAL_STATE, action) => {
