4a664b3e900ed180562240140c0e3fbbdd338a67
Oliver Stigley <oliverstigley@gmail.com>
2016-06-07 19:20:01 +1200

    refactor

--- app/reducer.js
+++ app/reducer.js
@@ -12,36 +12,42 @@ const INITIAL_STATE = fromJS({
   Total: "$0.00"
 })
 
-// const getTotal = (cart) => {
-//   let totals = 0
-//   console.log(state.keys())
 
-// }
 export default (state = INITIAL_STATE, action) => {
-  let total = 0
-  const products = state.get('products').filter( p => {
-    return state.get('cart').has(p.get('id').toString())
-  })
-  products.map( p => {
-    total += state.get('cart').get(p.get('id').toString())
-     * p.get('price')
-  })
-  console.log("total", total)
+  const total = () => {
+    let total =0
+    state.get('products')
+    .filter( p => {
+      return state.get('cart')
+        .has(p.get('id').toString())
+    })
+    .map( p => {
+      total += state.get('cart')
+        .get(p.get('id').toString())
+        * p.get('price')
+    })
+    return total
+  }
+  
+  
+  console.log("total", total())
 
   switch(action.type) {
     case 'ADD_PRODUCT_TO_CART':
       let productId = action.id.toString()
-      return state.updateIn(['cart', productId], 1, x => x + 1)
+      return state
+        .updateIn(['cart', productId], 1, x => x + 1)
+        .updateIn(['Total'], total)
     case 'REMOVE_PRODUCT_FROM_CART':
-      return (state.get('cart').get(action.id.toString()) == 1) ? state.deleteIn(['cart',action.id.toString()])      
-      : state.updateIn(
-        ['cart', 
-        action.id.toString()], 
-        0, 
-        x => x===0 ? x: x - 1)
+      return (state.get('cart').get(action.id.toString()) == 1) ? 
+        state
+          .deleteIn(['cart',action.id.toString()])
+          .updateIn(['Total'], total)      
+        :state
+          .updateIn(['cart', action.id.toString()],0,x => x===0 ? x: x - 1)
+          .updateIn(['Total'], total)
 
     default:
-      console.log(state.get('products').toObject())
       return state
   }
 }
